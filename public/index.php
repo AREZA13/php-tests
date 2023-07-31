5858c52e1cd8b01cd091913c67e65f52f77bdcfc
areza13 <arezasoft@icloud.com>
2023-07-31 13:39:02 -0400

    New tasks done in index.php

--- public/index.php
+++ public/index.php
@@ -110,3 +110,161 @@ function array_double2(array $array): array
 }
 
 assert(array_double([1, 2, 3, 4]) == [2, 4, 6, 8]);
+
+
+/*Напишите функцию divisible_by_three(int $max, int $min): array, которая формирует убывающий массив от
+$max и до $min из чисел, которые делятся на 3 без отстатка. В тестах проверьте что первый,
+последний и любой некрайний элемент списка действительно делятся на 3. Например для
+three_devided_range(1002, 0) вернет массив [1002, 999, ... 0]*/
+
+function divisible_by_three(int $max, int $min): array
+{
+    $sortedNumbers = array();
+    foreach (range($max, $min) as $number) {
+        array_push($sortedNumbers, $number);
+    }
+
+    $three_divided_range = array();
+    foreach ($sortedNumbers as $number) {
+        if ($number % 3 == 0) {
+            array_push($three_divided_range, $number);
+        }
+
+    }
+    return $three_divided_range;
+}
+
+/*print_r(divisible_by_three(1002, 0));*/
+
+
+/*Напишите функцию count_even(array $arr): int,
+которая считает количество четных чисел в массиве.
+В массиве [1, 2, 3] - только 1 четный элемент.*/
+
+function count_even(array $arr): int
+{
+    $count = 0;
+    foreach ($arr as $num) {
+        if ($num % 2 == 0) {
+            $count++;
+        }
+    }
+    return $count;
+}
+
+/*echo count_even([1, 2, 3, 4, 5, 6]);*/
+
+
+/*Напишите функцию min_even(array $arr): int
+Найдите наименьший четный(по значению) элемент массива.
+В массиве [1, 2, 3, 4] - 2 наименьший четный элемент.*/
+
+function min_even(array $arr): int
+{
+    $arrayWithEven = array();
+    foreach ($arr as $num) {
+        if ($num != 0 && $num % 2 == 0) {
+            array_push($arrayWithEven, $num);
+        }
+    }
+    $min = min($arrayWithEven);
+    return $min;
+}
+
+/*echo min_even([1, 2, 4, 5, 3, 2, 3, 0]);*/
+
+
+/*Напишите функцию min_sum_elements(array $arr): array
+    которая возвращает два соседних элемента, сумма которых минимальна.
+В массиве [1, 2, 3, 4] это элементы [1, 2].*/
+
+
+function min_sum_elements(array $arr): array
+{
+    $minSum = $arr[0] + $arr[1];
+    $minPara = [$arr[0], $arr[1]];
+    for ($i = 1; $i < count($arr) - 1; $i++) {
+        $sum = $arr[$i] + $arr[$i + 1];
+        if ($sum < $minSum) {
+            $minSum = $sum;
+            $minPara = [$arr[$i], $arr[$i + 1]];
+        }
+    }
+    return $minPara;
+}
+
+/*print_r(min_sum_elements([1, 2, 4, 5, 12, 342, 5, 0, 1]));*/
+
+/*Написать рекурсивную функцию sumn рассчета суммы 1 + 2 + 3 + ... + n.*/
+
+function sumn(int $n)
+{
+    if ($n == 1) {
+        return 1;
+    }
+    return $n + sumn($n - 1);
+}
+
+assert(sumn(2) == 3);
+assert(sumn(3) == 6);
+
+/*echo sumn(2);*/
+
+
+/*Написать функцию add_item($arr, $item),
+которая ничего не возвращает, но при этом добавляет в конец массива $arr элемент $item*/
+
+$arr = [1, 2, 3, 4];
+function add_item(&$arr, $item)
+{
+    array_push($arr, $item);
+
+}
+
+add_item($arr, 10);
+/*print_r($arr);*/
+
+/* Написать функцию `create_min_max_validator(int $min, int $max)` которая возвращает функцию принимающую один аргумент,
+функция проверяет входит ли аргумент в диапазон от $min до $max включительно.*/
+
+function create_min_max_validator(int $min, int $max)
+{
+    return function ($inputNumber) use ($min, $max) {
+        if ($inputNumber <= $max && $inputNumber >= $min) {
+            echo "Number ($inputNumber) is in range" . "<br>";
+        } else {
+            echo "Number ($inputNumber) is not in range" . "<br>";
+        }
+    };
+
+}
+
+/*$validator = create_min_max_validator(2, 5);
+$validator(10);
+$validator(2);
+$validator(3);*/
+
+
+/* Создать файл index.php и директорию `classes` в которой создать файлы `Test1.php` и  `Test2.php`.
+В каждом файле разместить одноименный класс с методом `do()`.
+В файле index.php написать функцию автолоадер и не используя `require` и `include` создать экземпляры каждого из классов и запустить метод do()*/
+
+spl_autoload_register(function ($className) {
+    $FilePath ='classes/' . $className . '.php';
+    if (file_exists($FilePath)) {
+        require_once $FilePath;
+    }
+});
+
+
+/*$test1 = new \application\classes\Test1();
+$test1->do();
+
+$test2 = new \application\classes\Test2();
+$test2->do();*/
+
+
+/*2. Создать файл index.php и директорию `src` в которой размещать классы соответственно стандарту автозагрузки PSR-4,
+в каждом классе должен быть метод do. `Автозагрузчик сгенерировать при помощи `composer.
+В файле index.php добавить use для таких классов `One\Test`, `Two\Test`, `Three\Four\Test` создать экземпляры всех классов
+и вызвать для них метод `do`, чтобы не было конфликта имен использовать псевдонимы Test1, Test2, Test3 соответственно.*/
