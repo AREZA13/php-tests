97bc693d55b779756ed55bcec30c7d1af674e86b
AREZA13 <88317106+AREZA13@users.noreply.github.com>
2024-05-15 18:31:52 +0300

    Update 3.php
    //variant with declarative paradigm

--- public/tasks/3.php
+++ public/tasks/3.php
@@ -27,3 +27,9 @@ function array_double2(array $array): array
 //var_dump(array_double2([1, 2, 3, 4]));
 var_dump(assert(array_double2([1, 2, 3, 4]) == [2, 4, 6, 8]));
 //php -d assert.active=1 -d assert.exception=1 3.php
+
+//With declarative method
+function array_double2(array $array): array
+{
+    return array_map(static fn($value) => $value * 2, $array );
+}
