4f8cc02a6b80c7f6916ce19f77c06a20f7b78fba
AREZA13 <88317106+AREZA13@users.noreply.github.com>
2024-05-17 14:53:02 +0300

    Update 2.php
    Added more variant of solving task even_to_zero

--- public/tasks/2.php
+++ public/tasks/2.php
@@ -75,7 +75,7 @@ function evenToZero3(int $n): int // [1234567]
 }
 print("evenToZero3 result is: " . evenToZero3(1234567) . PHP_EOL);
 
-//Declarative paradigm  ! (Problems with big numbers!!!! TODO
+//Declarative paradigm  ! (Problems with big numbers!!!! 
 function evenToZeroWithDeclarativeParadigm(int $number): int
 {
     return (int)implode("", array_map(
@@ -86,3 +86,58 @@ function evenToZeroWithDeclarativeParadigm(int $number): int
         str_split($number)
     ));
 }
+
+
+
+//can be applyied for any numbers (using logarithm)
+function evenToZero4(int $n): int // [1234567]
+{   if ($n % 2 === 0) {
+    $countNumber = floor(log10($n)) + 1; // 6+
+    $arrayFilledWithZeros = array_fill(0, $countNumber, 0); // [0,0,0,0,0,0]
+    $return_value = 0;
+    foreach ($arrayFilledWithZeros as $key => $value) {
+        // $key = 1 вторая итерация
+        $exponent = $key + 1; // 2
+        $divider = 10 ** $exponent; // 100
+        $new_value = $n % $divider; // 123'456 % 100 = 56
+        $new_value = floor($new_value / 10 ** $key); // 56 / 10 ~ 5
+        $arrayFilledWithZeros[$key] = $new_value;
+        $return_value += ($key % 2) ? $new_value * 10 ** $key : 0;
+    }
+    return $return_value;
+    }
+
+        $n *= 10;
+        $countNumber = floor(log10($n)) + 1;
+        $arrayFilledWithZeros = array_fill(0, $countNumber, 0); // [0,0,0,0,0,0]
+        $return_value = 0;
+        foreach ($arrayFilledWithZeros as $key => $value) {
+            // $key = 1 вторая итерация
+            $exponent = $key + 1; // 2
+            $divider = 10 ** $exponent; // 100
+            $new_value = $n % $divider; // 123'456 % 100 = 56
+            $new_value = floor($new_value / 10 ** $key); // 56 / 10 ~ 5
+            $arrayFilledWithZeros[$key] = $new_value;
+            $return_value += ($key % 2) ? $new_value * 10 ** $key : 0;
+        }
+
+        return $return_value/10;
+
+}
+
+print("evenToZero4 result is: " . evenToZero4(123456455832167) . PHP_EOL);
+
+
+//Declarative paradigm  ! (Can be any large number not only int size) 
+function evenToZeroWithDeclarativeParadigm2(int $n): int
+{
+    return ($n % 2 === 0) ?
+        (array_reduce(array_keys(array_fill(0, floor(log10($n)) + 1, 0)), static function ($carry, $key) use ($n) {
+        return $carry + (($key % 2) ? floor($n % 10 ** ($key + 1) / 10 ** $key) * 10 ** $key : 0);
+    }, 0)) :
+        ((array_reduce(array_keys(array_fill(0, floor(log10($n*10)) + 1, 0)), static function ($carry, $key) use ($n) {
+        return $carry + (($key % 2) ? floor($n*10 % 10 ** ($key + 1) / 10 ** $key) * 10 ** $key : 0);
+}, 0)))/10;
+}
+print("WTF result is: " . evenToZeroWithDeclarativeParadigm2(123456789) . PHP_EOL);
+
